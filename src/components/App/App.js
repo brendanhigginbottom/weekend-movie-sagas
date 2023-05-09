5250bc432ab6b31010c62d5e3d54a225f7f40af4
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-05-08 20:21:17 -0500

    minor adjustments and fun with vim

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,12 +10,10 @@ function App() {
 
   return (
     <div className="App">
-      <h1>The Movies Saga!</h1>
       <Router>        
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
         {/* Details page */}
         <Route exact path="/details">
           <Details />
